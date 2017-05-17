ceb01461271c8e61c913363d21a8b414a4665421
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2017-05-17 19:43:41 +0800

    config browser sync

--- webpack/prod.js
+++ webpack/prod.js
@@ -16,28 +16,28 @@ module.exports = {
         comments: false
       },
       compress: {
-        unsafe_comps: true,
-        properties: true,
-        keep_fargs: false,
-        pure_getters: true,
+        booleans: true,
+        cascade: true,
         collapse_vars: true,
-        unsafe: true,
-        warnings: false,
-        screw_ie8: true,
-        sequences: true,
-        dead_code: true,
-        drop_debugger: true,
         comparisons: true,
         conditionals: true,
+        dead_code: true,
+        drop_console: true,
+        drop_debugger: true,
         evaluate: true,
-        booleans: true,
-        loops: true,
-        unused: true,
         hoist_funs: true,
         if_return: true,
         join_vars: true,
-        cascade: true,
-        drop_console: true
+        keep_fargs: false,
+        loops: true,
+        properties: true,
+        pure_getters: true,
+        screw_ie8: true,
+        sequences: true,
+        unsafe: true,
+        unsafe_comps: true,
+        unused: true,
+        warnings: false
       }
     }),
     // HTML optimize
@@ -56,6 +56,10 @@ module.exports = {
         minifyURLs: true
       },
       inject: true
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
     })
   ]
 }
